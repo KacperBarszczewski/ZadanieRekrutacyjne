dd2626224b616b4ac3f7429f96bf2e6986f0fde2
KacperB <kacpero.barszczewski@gmail.com>
2024-09-19 19:39:55 +0200

    add filter

--- src/app/components/table-of-elements/table-of-elements.component.ts
+++ src/app/components/table-of-elements/table-of-elements.component.ts
@@ -1,27 +1,30 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { PeriodicElement, PeriodicElementService } from '../../services/periodic-element.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { EditElementPopupComponent } from '../edit-element-popup/edit-element-popup.component';
 import { MatDialog } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'table-of-elements',
   standalone: true,
-  imports: [MatTableModule, MatProgressSpinnerModule, EditElementPopupComponent],
+  imports: [MatTableModule, MatProgressSpinnerModule, EditElementPopupComponent, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './table-of-elements.component.html',
   styleUrl: './table-of-elements.component.scss'
 })
 export class TableOfElementsComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource: PeriodicElement[] = [];
+  dataSource: MatTableDataSource<PeriodicElement> = new MatTableDataSource<PeriodicElement>([]);
   readonly dialog = inject(MatDialog);
 
   constructor(private elementService: PeriodicElementService) { }
 
   ngOnInit(): void {
     this.elementService.getElements().subscribe(data => {
-      this.dataSource = data;
+      this.dataSource = new MatTableDataSource(data);
     });
   }
 
@@ -39,6 +42,11 @@ export class TableOfElementsComponent implements OnInit {
         })
       }
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
